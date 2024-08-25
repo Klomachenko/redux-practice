59b7d91fdce4f1fda3d3b36fa55698960ac5d15c
Klomachenko <leekyumin0901@naver.com>
2024-08-26 03:33:56 +0900

    feat: redux payload 실습

--- src/App.js
+++ src/App.js
@@ -5,16 +5,26 @@ import Box from './component/Box';
 
 function App() {
   const count = useSelector((state) => state.count);
+  const id = useSelector((state) => state.id);
+  const password = useSelector((state) => state.password);
   const dispatch = useDispatch();
 
   const increase = () => {
-    dispatch({ type: 'INCREMENT' });
+    dispatch({ type: 'INCREMENT', payload: { num: 5 } });
+  };
+
+  const login = () => {
+    dispatch({ type: 'LOGIN', payload: { id: '규민', password: 1234 } });
   };
 
   return (
     <div>
+      <h1>
+        {id}, {password}
+      </h1>
       <h1>{count}</h1>
       <button onClick={increase}>증가</button>
+      <button onClick={login}>Login</button>
       <Box />
     </div>
   );
